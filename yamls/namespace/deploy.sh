7c2fdf3134f36f6b8802ad179a0ab16c40063523
Scott Crawford <scrawford@digitalocean.com>
2019-06-21 15:34:52 -0400

    exit 0

--- yamls/namespace/deploy.sh
+++ yamls/namespace/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-kubectl apply -f 'https://raw.githubusercontent.com/scottcrawford03/k8s_yamls/master/yamls/namespace.yaml'
+kubectl apply -f https://raw.githubusercontent.com/scottcrawford03/k8s_yamls/master/yamls/metrics.yaml
+
+exit 0
